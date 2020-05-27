a754988a17355efa10c501ab59f8f3e823a8ff8d
李澈 <li.che@eland.co.kr>
2020-05-28 01:28:02 +0800

    布局改为做菜单

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
 import React, { Suspense, useState, useEffect } from "react";
-import { Menu } from "antd";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-
+import { Menu, Layout } from "antd";
+import {
+  DesktopOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+import './App.css'
 import Home from "./pages/home";
 import List from "./pages/list";
-
+const { Sider, Content } = Layout;
 
 const menus = [
   {
     key: "react",
     route: "/",
     title: "主页",
+    icon: <DesktopOutlined />,
   },
   {
     key: "react-list",
     route: "/list",
     title: "列表页",
+    icon: <UserOutlined />,
   }
 ];
 
@@ -23,6 +29,8 @@ const BASE_NAME = window.__POWERED_BY_QIANKUN__ ? "/react" : "";
 const App = () => {
   const [refresh, setRefresh] = useState(0);
   const [selectedKeys, setSelectKeys] = useState(["react"]);
+  const [collapsed, setCollapsed] = useState(false); // 折叠做菜单开关
+
   useEffect(() => {
     const { pathname } = window.location;
     const menu = menus.find(
@@ -33,26 +41,36 @@ const App = () => {
   }, [refresh]);
 
   return (
-    <Router basename={BASE_NAME}>
-      <section>
-        <Menu
-          onClick={() => setRefresh((refresh) => ++refresh)}
-          selectedKeys={selectedKeys}
-          mode="horizontal"
-        >
-          {menus.map((item) => (
-            <Menu.Item key={item.key}>
-              <Link to={item.route}>{item.title}</Link>
-            </Menu.Item>
-          ))}
-        </Menu>
-        <Suspense fallback={null}>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/list" component={List} />
-          </Switch>
-        </Suspense>
-      </section>
+    <Router basename={BASE_NAME} >
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider collapsible collapsed={collapsed} onCollapse={b => setCollapsed(b)}>
+          <Menu
+            onClick={() => setRefresh((refresh) => ++refresh)}
+            selectedKeys={selectedKeys}
+            mode="inline"
+            theme="dark"
+          >
+            {menus.map((item) => (
+              <Menu.Item key={item.key} >
+                <Link to={item.route} style={{ color: '#fff' }}>
+                  {item.icon}
+                  {!collapsed && item.title}
+                </Link>
+              </Menu.Item>
+            ))}
+          </Menu>
+        </Sider>
+        <Layout>
+          <Content style={{ margin: '0 0' }}>
+            <Suspense fallback={null}>
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/list" component={List} />
+              </Switch>
+            </Suspense>
+          </Content>
+        </Layout>
+      </Layout>
     </Router>
   );
 };
